3b924427bbd3bcb5088a0ae3b170fb5fe6c8b6db
Gabriel Almeida <gabriel.arau.almeida@gmail.com>
2026-08-31 23:53:50 -0300

    Criado a classe Livro com seus objetos definidos

--- domain/src/main/java/com/minhaestante/domain/com.minhaestante.domain.modelo/Livro.java
+++ domain/src/main/java/com/minhaestante/domain/com.minhaestante.domain.modelo/Livro.java
@@ -1,3 +1,38 @@
 public class Livro {
+    private String titulo;
+    private String sinopse;
+    private String autor;
+    private byte capa[];
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public byte[] getCapa() {
+        return capa;
+    }
+
+    public void setCapa(byte[] capa) {
+        this.capa = capa;
+    }
 }
