6f399feb231afb6f1dfae7c3e7b14008a872963e
Gabriel Almeida <gabriel.arau.almeida@gmail.com>
2026-09-01 22:43:19 -0300

    teste livro upload capa!

--- domain/src/main/java/com/minhaestante/domain/config/LivroRepository.java
+++ domain/src/main/java/com/minhaestante/domain/config/LivroRepository.java
@@ -4,13 +4,34 @@ import com.minhaestante.domain.model.Livro;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class LivroRepository {
-    private List <Livro> livros = new ArrayList<>(
-             List.of(new Livro("Entendendo Algoritmos", "Aditya Y. Bhargava", "9788575225639", null)));
 
-        public void cadastrasLivro(Livro livro) {
-            this.livros.add(livro);
+    private byte[] carregarCapaDoDisco(String caminhoDaImagem) {
+        try {
+            File arquivoImagem = new File(caminhoDaImagem);
+            return Files.readAllBytes(arquivoImagem.toPath());
+        } catch (IOException e) {System.err.println("Erro ao carregar a imagem da capa: " + e.getMessage());
+            return null;
+        }
+    }
+    private List<Livro> livros = new ArrayList<>(List.of(
+            new Livro(
+                    "Entendendo Algoritmos",
+                    "Um guia ilustrado para programadores",
+                    "Aditya Y. Bhargava",
+                    carregarCapaDoDisco("D:\\RenanGabriel\\domain\\src\\main\\java\\com\\minhaestante\\domain\\img\\imgteste.jpg")
+            )
+    ));
+
+    //private List <Livro> livros = new ArrayList<>(
+     //        List.of(new Livro("Entendendo Algoritmos", "Aditya Y. Bhargava", "9788575225639", null)));
+
+       public void cadastrasLivro(Livro livro) {
+           this.livros.add(livro);
         }
 
         public List<Livro> getLivros() {
