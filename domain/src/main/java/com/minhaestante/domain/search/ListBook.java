292d4866ed31adefbb1a814856e94d91e529463f
Gabriel Almeida <gabriel.arau.almeida@gmail.com>
2026-09-09 02:03:25 -0300

    Modulo de busca feito!

--- domain/src/main/java/com/minhaestante/domain/search/ListBook.java
+++ domain/src/main/java/com/minhaestante/domain/search/ListBook.java
@@ -23,4 +23,23 @@ public class ListBook {
             System.out.println(livro);
         }
     }
+
+    public void buscarBook (String livro){
+        if (livros.isEmpty()){
+            System.out.println("Não a livros na sua estate!");
+            return;
+        }
+
+        boolean encontrado = false;
+
+        for (int i = 0; i < livros.size(); i++) {
+            if (livros.get(i).getTitulo().toLowerCase().contains(livro.toLowerCase())) {
+                System.out.println("Livro encontrado: " + livros.get(i));
+                encontrado = true;
+            }
+        }
+        if (!encontrado){
+            System.out.println("Nenhum livro encontrado!" + livro);
+        }
+    }
 }
--- domain/src/main/java/com/minhaestante/domain/search/SearchBook.java
+++ domain/src/main/java/com/minhaestante/domain/search/SearchBook.java
@@ -7,23 +7,6 @@ import java.util.Locale;
 
 public class SearchBook {
 
-    public void buscarBook (ArrayList<Livro> livros, String livro){
-        if (livros.isEmpty()){
-            System.out.println("Não a livros na sua estate!");
-            return;
-        }
 
-        boolean encontrado = false;
-
-        for (int i = 0; i < livros.size(); i++) {
-            if (livros.get(i).getTitulo().toLowerCase().contains(livro.toLowerCase())){
-                System.out.println("Livro encontrado: " + livros.get(i));
-                encontrado = true;
-            }
-            if (!encontrado){
-                System.out.println("Nenhum livro encontrado!" + livro);
-            }
-        }
-    }
 
 }
